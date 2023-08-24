345fbfd5cf3c6e438b9edee7d9511d7c98116979
Miraaj Kadam <miraajkadam@gmail.com>
2023-08-24 10:16:10 +0530

    Session name configurable

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
 import { coBrowsing } from 'sariska-cobrowsing'
+import './App.css'
+import logo from './logo.svg'
 
 function App() {
-  // coBrowsing.startCoBrowsing("username", "sessionname");
+  const [sessionName, setSessionName] = useState('')
+
+  useEffect(() => {
+    return () => {
+      coBrowsing.stopCoBrowsing()
+    }
+  }, [])
+
+  const startSession = () => {
+    if (sessionName.trim().length === 0) alert('Please enter a session name')
+
+    coBrowsing.startCoBrowsing(`User${Math.round(Math.random(100) * 100)}`, sessionName)
+  }
 
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
+        <button
+          onClick={() => {
+            startSession()
+          }}
+        >
+          START SESSION
+        </button>
+        <input
+          type='text'
+          value={sessionName}
+          onChange={e => {
+            const { value } = e.target
+
+            setSessionName(value)
+          }}
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
